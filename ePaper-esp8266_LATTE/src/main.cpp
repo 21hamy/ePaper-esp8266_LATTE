c7ebbdc2cb0b99d648e39faf762803f0bab115c4
CMMC\clicknectpc <apiruk_sang-ngenchai@hotmail.com>
2018-06-12 13:25:53 +0700

    Code ePaper 4.2inch 400x300 3 color

---
+++ ePaper-esp8266_LATTE/src/main.cpp
@@ -0,0 +1,106 @@
+// GxEPD_SPI_TestExample : test example for e-Paper displays from Waveshare and from Dalian Good Display Inc.
+//
+// Created by Jean-Marc Zingg based on demo code from Good Display,
+// available on http://www.good-display.com/download_list/downloadcategoryid=34&isMode=false.html
+//
+// The e-paper displays are available from:
+//
+// https://www.aliexpress.com/store/product/Wholesale-1-54inch-E-Ink-display-module-with-embedded-controller-200x200-Communicate-via-SPI-interface-Supports/216233_32824535312.html
+//
+// http://www.buy-lcd.com/index.php?route=product/product&path=2897_8363&product_id=35120
+// or https://www.aliexpress.com/store/product/E001-1-54-inch-partial-refresh-Small-size-dot-matrix-e-paper-display/600281_32815089163.html
+//
+
+// Supporting Arduino Forum Topics:
+// Waveshare e-paper displays with SPI: http://forum.arduino.cc/index.php?topic=487007.0
+// Good Dispay ePaper for Arduino : https://forum.arduino.cc/index.php?topic=436411.0
+
+// mapping suggestion from Waveshare SPI e-Paper to Wemos D1 mini
+// BUSY -> D2, RST -> D4, DC -> D3, CS -> D8, CLK -> D5, DIN -> D7, GND -> GND, 3.3V -> 3.3V
+
+// mapping suggestion from Waveshare SPI e-Paper to generic ESP8266
+// BUSY -> GPIO4, RST -> GPIO2, DC -> GPIO0, CS -> GPIO15, CLK -> GPIO14, DIN -> GPIO13, GND -> GND, 3.3V -> 3.3V
+
+// mapping suggestion for ESP32, e.g. LOLIN32, see .../variants/.../pins_arduino.h for your board
+// NOTE: there are variants with different pins for SPI ! CHECK SPI PINS OF YOUR BOARD
+// BUSY -> 4, RST -> 16, DC -> 17, CS -> SS(5), CLK -> SCK(18), DIN -> MOSI(23), GND -> GND, 3.3V -> 3.3V
+
+// new mapping suggestion for STM32F1, e.g. STM32F103C8T6 "BluePill"
+// BUSY -> A1, RST -> A2, DC -> A3, CS-> A4, CLK -> A5, DIN -> A7
+
+// mapping suggestion for AVR, UNO, NANO etc.
+// BUSY -> 7, RST -> 9, DC -> 8, CS-> 10, CLK -> 13, DIN -> 11
+
+// include library, include base class, make path known
+#include <GxEPD.h>
+
+// select the display class to use, only one
+//#include <GxGDEP015OC1/GxGDEP015OC1.cpp>    // 1.54" b/w
+//#include <GxGDEW0154Z04/GxGDEW0154Z04.cpp>  // 1.54" b/w/r 200x200
+//#include <GxGDEW0154Z17/GxGDEW0154Z17.cpp>  // 1.54" b/w/r 152x152
+//#include <GxGDE0213B1/GxGDE0213B1.cpp>      // 2.13" b/w
+//#include <GxGDEW0213Z16/GxGDEW0213Z16.cpp>  // 2.13" b/w/r
+//#include <GxGDEH029A1/GxGDEH029A1.cpp>      // 2.9" b/w
+//#include <GxGDEW029Z10/GxGDEW029Z10.cpp>    // 2.9" b/w/r
+//#include <GxGDEW027C44/GxGDEW027C44.cpp>    // 2.7" b/w/r
+//#include <GxGDEW027W3/GxGDEW027W3.cpp>      // 2.7" b/w
+//#include <GxGDEW042T2/GxGDEW042T2.cpp>      // 4.2" b/w
+#include <GxGDEW042Z15/GxGDEW042Z15.cpp>    // 4.2" b/w/r
+//#include <GxGDEW0583T7/GxGDEW0583T7.cpp>    // 5.83" b/w
+//#include <GxGDEW075T8/GxGDEW075T8.cpp>      // 7.5" b/w
+//#include <GxGDEW075Z09/GxGDEW075Z09.cpp>    // 7.5" b/w/r
+
+
+#include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
+#include <GxIO/GxIO.cpp>
+
+#if defined(ESP8266)
+
+// generic/common.h
+//static const uint8_t SS    = 15; // D8
+//static const uint8_t MOSI  = 13; // D7
+//static const uint8_t MISO  = 12; // D6
+//static const uint8_t SCK   = 14; // D5
+
+GxIO_Class io(SPI, /*CS=D8*/ SS, /*DC=D3*/ 0, /*RST=D4*/ 2); // arbitrary selection of D3(=0), D4(=2), selected for default of GxEPD_Class
+GxEPD_Class display(io /*RST=D4*/ /*BUSY=D2*/); // default selection of D4(=2), D2(=4)
+// Heltec E-Paper 1.54" b/w without BUSY
+//GxEPD_Class display(io, /*RST=D4*/ 2, /*BUSY=D2*/ -1); // default selection of D4(=2), no BUSY
+
+#else
+
+// pins_arduino.h, e.g. AVR
+//#define PIN_SPI_SS    (10)
+//#define PIN_SPI_MOSI  (11)
+//#define PIN_SPI_MISO  (12)
+//#define PIN_SPI_SCK   (13)
+
+GxIO_Class io(SPI, /*CS=*/ SS, /*DC=*/ 0, /*RST=*/ 2); // arbitrary selection of 8, 9 selected for default of GxEPD_Class
+GxEPD_Class display(io /*RST=9*/ /*BUSY=7*/); // default selection of (9), 7
+
+#endif
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("setup");
+
+  display.init(115200); // enable diagnostic output on Serial
+
+  Serial.println("setup done"); 
+}
+
+void loop()
+{
+  display.fillScreen(GxEPD_WHITE);
+  display.setTextColor(GxEPD_BLACK);
+  display.setTextSize(10);
+  display.setCursor(90, 125);
+  display.println("CMMC");
+  display.update();
+  delay(10000);
+  loop();
+
+}
